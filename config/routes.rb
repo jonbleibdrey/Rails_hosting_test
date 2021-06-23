5f6a17bfe629994ee3bfdaeeee554866c2a82b7d
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-06-23 17:08:55 -0400

    added root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: "user#index"
+  get "/" to: "user#index"
   resources :recipes
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
